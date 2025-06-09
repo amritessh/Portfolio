32374d4c413332c2c6dc085db32a16da23a48e44
Amritesh Anand <amriteshanand7@gmail.com>
2025-06-08 23:07:22 -0400

    fixing imports

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 import Footer from './components/common/Footer';
 import { useDarkMode } from './hooks/useDarkMode';
-import { useScrollspy } from './hooks/useScrollspy';
+import { useScrollspy } from './hooks/useScrollSpy';
 import CalendlyBadge from './components/CalendlyBadge';
 
 function App() {
